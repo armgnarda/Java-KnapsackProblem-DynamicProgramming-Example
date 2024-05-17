3d718509d60bb47ff338b11599cb2c31851fed87
armgnarda <93938720+armgnarda@users.noreply.github.com>
2024-05-17 12:53:39 +0300

    Update KnapsackProblem.java

--- KnapsackProblem/src/KnapsackProblem.java
+++ KnapsackProblem/src/KnapsackProblem.java
@@ -7,17 +7,16 @@ public class KnapsackProblem {
 		int[][] dp = new int[n+1][W+1]; 
 		
 		for (int i = 0; i <= n; i++) {
-            for (int w = 0; w <= W; w++) {
-                if (i == 0 || w == 0) {
-                    dp[i][w] = 0;
-                } else if (wt[i - 1] <= w) {
-                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
-                } else {
-                    dp[i][w] = dp[i - 1][w];
-                }
-            }
-        }	
-		
+        		for (int w = 0; w <= W; w++) {
+                		if (i == 0 || w == 0) {
+                   			 dp[i][w] = 0;
+                		} else if (wt[i - 1] <= w) {
+                   			 dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+               			 } else {
+                   			 dp[i][w] = dp[i - 1][w];
+                		}
+            		}
+       		 }	
 		return dp[n][W];
 	}
 	public static void main(String[] args) {
@@ -28,7 +27,5 @@ public class KnapsackProblem {
 		int n = val.length;
 		
 		System.out.println("Maximum value that can be obtained: " + knapsack(W, wt, val, n));
-		
 	}
-
 }
